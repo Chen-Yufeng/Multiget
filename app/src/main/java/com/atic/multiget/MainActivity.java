8c1997b13658e54c8b177616e717dd7502d1bac1
IfChan <956472367@qq.com>
2017-09-30 20:50:53 +0800

    Start

---
+++ app/src/main/java/com/atic/multiget/MainActivity.java
@@ -0,0 +1,13 @@
+package com.atic.multiget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
